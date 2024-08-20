27a23d24615ba19b14b3f52da70436ffff498292
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2024-08-20 12:15:55 -0300

    projeto 1.0 finalizado

--- src/front-end/main.js
+++ src/front-end/main.js
@@ -1,7 +1,11 @@
 
 function getKartDataFromLS() {
     const storedLista = localStorage.getItem('KartData');
-    return storedLista ? JSON.parse(storedLista) : {};
+    try {
+        return storedLista ? JSON.parse(storedLista) : {};
+    } catch {
+        return {}
+    }
 }
 
 function savaKastDataToLS(lista) {
@@ -107,19 +111,83 @@ function atualizarListaGeral(){
 
     for (const key in lista) {
         tbody += `
-        <tr>
-            <td>${key}</td>
-            <td>${lista[key]['WARMUP']}</td>
-            <td>${lista[key]['COBRE I']}</td>
-            <td>${lista[key]['COBRE II']}</td>
-            <td>${lista[key]['BRONZE']}</td>
-            <td>${lista[key]['PRATA']}</td>
-            <td>${lista[key]['OURO']}</td>
+        <tr id="${key}">
+            <td class="kartKey">${key}</td>
+            <td class="WARMUP">${lista[key]['WARMUP']}</td>
+            <td class="COBRE-I">${lista[key]['COBRE I']}</td>
+            <td class="COBRE-II">${lista[key]['COBRE II']}</td>
+            <td class="BRONZE">${lista[key]['BRONZE']}</td>
+            <td class="PRATA">${lista[key]['PRATA']}</td>
+            <td class="OURO">${lista[key]['OURO']}</td>
         </tr>
         `
     const table = document.querySelector('#finalTable')
     table.querySelector('tbody').innerHTML = tbody
     }
 }
-
 atualizarListaGeral()
+
+function limpar(){
+    const rows = document.querySelectorAll('.selectedRows');
+    rows.forEach((element) => {
+        element.classList.remove('selectedRows')
+    })
+}
+
+function editar() {
+    document.querySelector('#finalTable').classList.add('editable');
+    setupEditar()
+}
+
+function baixarExcel() {
+    const tabela = document.getElementById('finalTable');
+    const wb = XLSX.utils.table_to_book(tabela);
+    XLSX.writeFile(wb, 'tabela.xlsx');
+}
+
+function setupEditar() {
+    document.querySelectorAll('.editable .kartKey').forEach((row) => {
+        row.addEventListener('mouseenter', (e) => {
+            if (e.target.parentNode.parentNode.parentNode.classList.contains('editable')) {
+                e.target.parentNode.style.backgroundColor = 'rgb(255, 205, 205)'
+            }
+        })
+        row.addEventListener('click', (e) => {
+            let resposta = confirm('Excluir campo?')
+            if (e.target.parentNode.parentNode.parentNode.classList.contains('editable') && resposta) {
+                let lista = getKartDataFromLS()
+
+                delete lista[e.target.parentNode.id]
+
+                savaKastDataToLS(lista)
+                atualizarListaGeral()
+
+                if (Object.keys(lista).length == 0){
+                    location.reload()
+                }
+            }
+        })
+        row.addEventListener('mouseleave', (e) => {
+            e.target.parentNode.style.backgroundColor = ''
+        })
+    })
+    document.querySelectorAll('.editable td').forEach((td) => {
+        td.addEventListener('mouseup', (e) => {
+            if (!e.target.classList.contains('kartKey')) {
+                let resposta = confirm('Excluir campo?')
+                if(resposta) {
+                    let lista = getKartDataFromLS()
+                    lista[e.target.parentNode.querySelector('.kartKey').textContent][e.target.classList[0].replace(/-/g, ' ')] = '';
+                    savaKastDataToLS(lista)
+                    atualizarListaGeral()
+                }
+            }
+        })
+    })
+    document.addEventListener('click', (e) => {
+        if (!document.querySelector('#divFT').contains(e.target)) {
+            document.querySelector('#finalTable').classList.remove('editable')
+        }
+    })
+    
+}
